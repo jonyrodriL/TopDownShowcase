00713a0ef2966da84da3453b95a6deb6b6de0f01
jonyrodriL <jonathanrodriguez0515@gmail.com>
2024-09-29 21:51:07 -0700

    adding EnenmyDamage

--- My project/Assets/Scripts/PlayerHealth.cs
+++ My project/Assets/Scripts/PlayerHealth.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField]
     int health = 10;
+    Image HealthBar;
+    float maxHealth;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxHealth = health;
+        HealthBar = GetComponentsInChildren<Image>()[1];
+        HealthBar.fillAmount = health / maxHealth;
+
     }
 
     // Update is called once per frame
@@ -19,7 +29,7 @@ public class PlayerHealth : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject.name);
+        //Debug.Log(collision.gameObject.name);
         //we want to take damage IF the player hits the enemy capsule
         //bool key = true;
         if (collision.gameObject.tag == "Enemy")
@@ -35,9 +45,20 @@ public class PlayerHealth : MonoBehaviour
         {
             health -= 1;
         }
+        if (health <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+       
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+       if(collision.gameObject.tag == "EnemyBullet")
+
+        {
+            health -= 1;
+        }
     }
+    
+
 }
